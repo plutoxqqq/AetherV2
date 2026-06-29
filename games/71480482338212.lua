9043e7cb41df5bf38896fd5c702ae71229a851d2
plutoxqqq <plutoxqq@gmail.com>
2026-06-30 08:44:09 +1000

    Add ProjectileExploit module

--- games/71480482338212.lua
+++ games/71480482338212.lua
@@ -5622,6 +5622,139 @@ run(function()
     })
 end)
 
+
+run(function()
+    local ProjectileExploit
+    local CustomProjectiles
+    local Targets
+    local Range
+    local List
+    local rayCheck = RaycastParams.new()
+    rayCheck.FilterType = Enum.RaycastFilterType.Include
+    local projectileRemote = {InvokeServer = function(self, ...) end}
+    local FireDelays = {}
+    task.spawn(function()
+	projectileRemote = bedwars.Client:Get(remotes.FireProjectile).instance
+    end)
+
+    local function getAmmo(check)
+	for _, item in store.inventory.inventory.items do
+		if check.ammoItemTypes and table.find(check.ammoItemTypes, item.itemType) then
+			return item.itemType
+		end
+	end
+	return nil
+    end
+
+    local function getProjectiles()
+	local items = {}
+	for _, item in store.inventory.inventory.items do
+		local itemMeta = bedwars.ItemMeta[item.itemType]
+		local proj = itemMeta and itemMeta.projectileSource
+		local ammo = proj and getAmmo(proj)
+		if ammo and table.find(List.ListEnabled, ammo) then
+			table.insert(items, {
+				item,
+				ammo,
+				proj.projectileType(ammo),
+				proj
+			})
+		end
+	end
+	return items
+    end
+
+    ProjectileExploit = vape.Categories.Blatant:CreateModule({
+	Name = 'ProjectileExploit',
+	Function = function(callback)
+		if callback then
+			repeat
+				if (workspace:GetServerTimeNow() - bedwars.SwordController.lastAttack) > 0.5 then
+					local ent = entitylib.EntityPosition({
+						Part = 'RootPart',
+						Range = Range.Value,
+						Players = Targets.Players.Enabled,
+						NPCs = Targets.NPCs.Enabled,
+						Wallcheck = Targets.Walls.Enabled
+					})
+
+					if ent then
+						local pos = entitylib.character.RootPart.Position
+						for _, data in getProjectiles() do
+							local item, ammo, projectile, itemMeta = unpack(data)
+							if (FireDelays[item.itemType] or 0) < tick() then
+								rayCheck.FilterDescendantsInstances = {workspace.Map}
+								if #CustomProjectiles.ListEnabled > 0 then
+									projectile = CustomProjectiles.ListEnabled[math.random(1, #CustomProjectiles.ListEnabled)]
+								end
+								local meta = bedwars.ProjectileMeta[projectile]
+								if not meta then
+									continue
+								end
+								local projSpeed, gravity = meta.launchVelocity, meta.gravitationalAcceleration or 196.2
+								local calc = prediction.SolveTrajectory(pos, projSpeed, gravity, ent.RootPart.Position, ent.RootPart.Velocity, workspace.Gravity, ent.HipHeight, ent.Jumping and 42.6 or nil, rayCheck)
+								if calc then
+									targetinfo.Targets[ent] = tick() + 1
+									local switched = switchItem(item.tool)
+
+									task.spawn(function()
+										local dir, id = CFrame.lookAt(pos, calc).LookVector, httpService:GenerateGUID(true)
+										local shootPosition = (CFrame.new(pos, calc) * CFrame.new(Vector3.new(-bedwars.BowConstantsTable.RelX, -bedwars.BowConstantsTable.RelY, -bedwars.BowConstantsTable.RelZ))).Position
+										bedwars.ProjectileController:createLocalProjectile(meta, ammo, projectile, shootPosition, id, dir * projSpeed, {drawDurationSeconds = 1})
+										local _, res = pcall(function()
+											return projectileRemote:InvokeServer(item.tool, ammo, projectile, shootPosition, pos, dir * projSpeed, id, {drawDurationSeconds = 1, shotId = httpService:GenerateGUID(false)}, workspace:GetServerTimeNow())
+										end)
+										if not res then
+											FireDelays[item.itemType] = tick()
+										else
+											local shoot = itemMeta.launchSound
+											shoot = shoot and shoot[math.random(1, #shoot)] or nil
+											if shoot then
+												bedwars.SoundManager:playSound(shoot)
+											end
+										end
+									end)
+
+									FireDelays[item.itemType] = tick() + itemMeta.fireDelaySec
+									if switched then
+										task.wait(0.05)
+									end
+								end
+							end
+						end
+					end
+				end
+				task.wait(0.1)
+			until not ProjectileExploit.Enabled
+		end
+	end,
+	Tooltip = 'Shoots people around you with custom projectile types'
+    })
+    Targets = ProjectileExploit:CreateTargets({
+	Players = true,
+	Walls = true
+    })
+    List = ProjectileExploit:CreateTextList({
+	Name = 'Projectiles',
+	Default = {'arrow', 'snowball'}
+    })
+    CustomProjectiles = ProjectileExploit:CreateTextList({
+	Name = 'Exploited Projectiles',
+	Default = {'meteor_shower'},
+	Placeholder = 'projectile'
+    })
+    Range = ProjectileExploit:CreateSlider({
+	Name = 'Range',
+	Min = 1,
+	Max = 50,
+	Default = 50,
+	Suffix = function(val)
+		return val == 1 and 'stud' or 'studs'
+	end
+    })
+end)
+
+
 run(function()
     local Mode
     local Value
